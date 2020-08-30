f967d84fbd947afad5d5c9472352ff0f9be7399c
Billy Chen <billychen10@hotmail.com>
2020-08-31 01:06:48 +1000

    Routing working
    - /create and /poll now route correctly
    - need to implement '/' default home page, with the routing buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,45 @@
 import React, {useState} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
 import './Home.css';
 import Board from './Board/Board';
 import CreatePoll from './CreatePoll/CreatePoll';
 
 const Home = () => {
 
-    const [page, setPage] = useState('');
-
     // Renders home page with selections to create a poll or retrieve a poll
     return (
-        <div>
+        <Router>
             <div className="home-container">
                 <div className="title">
                     Billy's Cool Poll!
                 </div>
                 <div className="content">
-                    { page == '' &&
-                        <div>
-                            <button className='home-select' onClick={() => setPage('createPoll')}> 
-                                Create Poll
-                            </button>
-                            <button className='home-select' onClick={() => setPage('getPoll')}>
-                                Connect to Poll
-                            </button>
-                        </div>
-                    }
+                    <div>
+                        <button className='home-select'> 
+                            <Link className='router-link' to="/create">Create Poll</Link>
+                        </button>
+                        <button className='home-select'>
+                            <Link className='router-link' to="/poll">Connect to Poll</Link>
+                        </button>
+                    </div>
 
-                    { page == 'createPoll' && <CreatePoll/> }
-                    { page == 'getPoll' && <Board />}
-
+                    <Switch>
+                        <Route path="/create">
+                            <CreatePoll />
+                        </Route>
+                        <Route path="/poll">
+                            <Board />
+                        </Route>
+                    </Switch>
                 </div>
             </div>
-        </div>  
+        </Router>  
     );
 }
 
